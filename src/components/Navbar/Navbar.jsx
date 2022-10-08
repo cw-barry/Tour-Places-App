5018a0c9f8984e89a60cf61afa9f5cf22bf59a28
cw-barry <barry@clarusway.com>
2022-10-08 19:44:33 -0400

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,25 @@
 import './Navbar.scss';
 import React from 'react';
 
+const menu = [
+  ['About Us', '#aboutus'],
+  ['For You', '#foryou'],
+  ['Services', '#services'],
+  ['Blog', '#blog'],
+  ['Vlog', '#vlog'],
+  ['Contact', '#contact'],
+];
 
 const Navbar = () => {
-    return(
-        <div className="navbar">
-            <a href="#aboutus">About Us</a>
-            <a href="#foryou">For You</a>
-            <a href="#services">Services</a>
-            <a href="#blog">Blog</a>
-            <a href="#vlog">Vlog</a>
-            <a href="#contact">Contact</a>
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      {menu.map((item, index) => (
+        <a key={index} href={item[1]}>
+          {item[0]}
+        </a>
+      ))}
+    </div>
+  );
+};
 
 export default Navbar;
-
