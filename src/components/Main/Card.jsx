f26a6646c83acb8f302f5227d77ca766d9b12bcc
cw-barry <barry@clarusway.com>
2022-10-08 19:47:27 -0400

    updated card and main

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -1,22 +1,17 @@
-
-
 const Card = (data) => {
-    console.log("ne geliyo", data);
-    const {id,title,image,desc} = data;
-    return(
-        <div className="cards">
-            <div className="title">
-                <h1>{title}</h1>
-            </div>
-            <img src={image} alt="" />
+  const { id, title, image, desc } = data;
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
+      <img src={image} alt="" />
 
-            <div className="card-over">
-                <p>
-                    {desc}
-                </p>
-            </div>
-        </div>
-    )
-}
+      <div className="card-over">
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Card;
